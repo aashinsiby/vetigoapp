2fccf640f94357b8af0d0f269d516b8859638812
aashinsiby <aashin.csa1923@saintgits.org>
2024-02-05 13:03:10 +0530

    1.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
@@ -11,17 +11,30 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { ForgetComponent } from './login/forget/forget.component';
 import { PdfComponent } from './pdf/pdf.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
+import { Observable } from 'rxjs';
+import { Firestore ,collection} from 'firebase/firestore';
+import { collectionData } from '@angular/fire/firestore';
+interface Item {
+  name: string,
+  
+};
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,RouterOutlet,LoginComponent,RouterLink,RouterLinkActive,NavbarComponent,MatButtonModule,MatIconButton,MatIconModule,
-    SignupComponent,AboutusComponent,MatTabsModule,ForgetComponent,PdfComponent,UserprofileComponent],
+    SignupComponent,AboutusComponent,MatTabsModule,ForgetComponent,PdfComponent,UserprofileComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'Vetigo';
-  showLogin = false;
+  item$: Observable<Item[]>;
+  firestore: Firestore = inject(Firestore);
 
-
+  constructor() {
+    const itemCollection = collection(this.firestore, 'items');
+    this.item$ = collectionData(itemCollection) as Observable<Item[]>;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
-  
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp( {
+      apiKey: "AIzaSyAkVjKQNGx19ZJxp-dUNuj7U8gVtTPVzTE",
+      authDomain: "vetigo-fc02e.firebaseapp.com",
+      databaseURL: "https://vetigo-fc02e-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "vetigo-fc02e",
+      storageBucket: "vetigo-fc02e.appspot.com",
+      messagingSenderId: "255874786930",
+      appId: "1:255874786930:web:3d5b5ba47e7d20a63b84eb",
+      measurementId: "G-6D8SCWYTHR"})),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
